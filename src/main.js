2bcf208226c2b212bf149984829d6375f27e89ac
naimao <1024175936@qq.com>
2018-11-18 01:45:14 +0800

    11.18晚提交 项目初始化完毕

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import "./assets/styles/reset.css"
+import "./assets/styles/border.css"
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
