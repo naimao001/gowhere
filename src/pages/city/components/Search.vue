d58c1ea1971c7e59a1d4ef670a399155326207f4
naimao <1024175936@qq.com>
2018-11-19 21:21:49 +0800

    城市选择的搜索框 待合并

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="searchBox">
+    <input type="search" class="search" placeholder="请输入城市">
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.searchBox {
+  height: 36px;
+  padding: 5px;
+  background-color: $bgColor;
+
+  .search {
+    width: 100%;
+    height: 31px;
+    text-align: center;
+    border-radius: 3px;
+    color: #666;
+    padding: 0 2px;
+  }
+}
+</style>
